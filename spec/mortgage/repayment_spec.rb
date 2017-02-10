4ef090382244f7a54d29402219eb37f49d8ef011
Mark Connell <mconnell@litmus.com>
2017-02-10 21:55:40 +0000

    repayment specs

---
+++ spec/mortgage/repayment_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+RSpec.describe Calculators::Mortgage::Repayment do
+  describe ".new" do
+    context "repayment of 500.00 on a 10000 loan at 0.5%" do
+      let(:principal) { BigDecimal.new("10000") }
+      let(:rate)      { BigDecimal.new("0.005") }
+      let(:payment)   { BigDecimal.new("500.00") }
+
+      subject { described_class.new(principal: principal, rate: rate, payment: payment) }
+
+      it "has a payment of 500.00" do
+        expect(subject.payment).to eq(BigDecimal.new("500.00"))
+      end
+
+      it "has an interest payment of 50.00" do
+        expect(subject.interest).to eq(BigDecimal.new("50.00"))
+      end
+
+      it "has a deduction from the principal of 450.00" do
+        expect(subject.deduction).to eq(BigDecimal.new("450.00"))
+      end
+
+      it "has a principal value of 9550.00, after payment + interest" do
+        expect(subject.principal).to eq(BigDecimal.new("9550.00"))
+      end
+    end
+  end
+end
